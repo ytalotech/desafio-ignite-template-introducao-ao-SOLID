f5fe61caef5bbba13ed4811c231f95d0afa76c8e
lopessoftec <lopessoftec@gmail.com>
2022-11-21 19:28:14 -0300

    Desafio realizado

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,16 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+      const all = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.json(all);
+    } catch (e) {
+      return response.status(400).json({ error: e.message });
+    }
   }
 }
 
